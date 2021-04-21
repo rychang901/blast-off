cd91a5d664aecb7090930a5046e97f6678793be7
rychang901 <rychang@gmail.com>
2021-04-21 15:46:45 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,210 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+namespace SpriteKind {
+    export const Jumper = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
-        mySprite.vy = -85
+        mySprite.vy = -95
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . . . . 
+            . . . . a a a a a a a . . . . 
+            . . a a a a a a a a a a a . . 
+            . a a a . . . . . . . a a a . 
+            . a a . . . . . . . . . a a . 
+            a a . . . . . . . . . . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            . a a . . . . . . . . . a a a 
+            . a a a . . . . . . . a a a . 
+            . 4 a a a a a a a a a a a 4 . 
+            . 4 2 4 a a a a a a a 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . . 2 4 . . . . . . . . 2 4 . 
+            . . . 4 . . . . . . . . . 4 . 
+            `,img`
+            . . . . . . . . . . . . . . . 
+            . . . . a a a a a a a . . . . 
+            . . a a a a a a a a a a a . . 
+            . a a a . . . . . . . a a a . 
+            . a a . . . . . . . . . a a . 
+            a a . . . . . . . . . . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            . a a . . . . . . . . . a a a 
+            . a a a . . . . . . . a a a . 
+            . 4 a a a a a a a a a a a 4 . 
+            . 4 2 4 a a a a a a a 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . 4 2 . . . . . . . . 4 2 . . 
+            . 4 . . . . . . . . . 4 . . . 
+            `],
+        250,
+        false
+        )
+        pause(501)
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . . . . 
+            . . . . a a a a a a a . . . . 
+            . . a a a a a a a a a a a . . 
+            . a a a . . . . . . . a a a . 
+            . a a . . . . . . . . . a a . 
+            a a . . . . . . . . . . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            . a a . . . . . . . . . a a a 
+            . a a a . . . . . . . a a a . 
+            . 4 a a a a a a a a a a a 4 . 
+            . 4 2 4 a a a a a a a 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . . 2 4 . . . . . . . . 2 4 . 
+            . . 2 . . . . . . . . . 2 4 . 
+            `,img`
+            . . . . . . . . . . . . . . . 
+            . . . . a a a a a a a . . . . 
+            . . a a a a a a a a a a a . . 
+            . a a a . . . . . . . a a a . 
+            . a a . . . . . . . . . a a . 
+            a a . . . . . . . . . . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . a . . . . . a . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            a a . . . . . . . . . . . a a 
+            . a a . . . . . . . . . a a a 
+            . a a a . . . . . . . a a a . 
+            . 4 a a a a a a a a a a a 4 . 
+            . 4 2 4 a a a a a a a 4 2 4 . 
+            . 4 2 4 . . . . . . . 4 2 4 . 
+            . 4 2 4 . . . . . . . . 2 4 . 
+            . 4 2 4 . . . . . . . . 2 4 . 
+            . . 2 4 . . . . . . . . 2 . . 
+            `],
+        250,
+        true
+        )
     }
 })
+scene.onOverlapTile(SpriteKind.Jumper, assets.tile`myTile1`, function (sprite, location) {
+    mySprite.vy = -100
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . 
+        . . . . a a a a a a a . . . . 
+        . . a a a a a a a a a a a . . 
+        . a a a . . . . . . . a a a . 
+        . a a . . . . . . . . . a a . 
+        a a . . . . . . . . . . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        . a a . . . . . . . . . a a a 
+        . a a a . . . . . . . a a a . 
+        . 4 a a a a a a a a a a a 4 . 
+        . 4 2 4 a a a a a a a 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . . 2 4 . . . . . . . . 2 4 . 
+        . . . 4 . . . . . . . . . 4 . 
+        `,img`
+        . . . . . . . . . . . . . . . 
+        . . . . a a a a a a a . . . . 
+        . . a a a a a a a a a a a . . 
+        . a a a . . . . . . . a a a . 
+        . a a . . . . . . . . . a a . 
+        a a . . . . . . . . . . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        . a a . . . . . . . . . a a a 
+        . a a a . . . . . . . a a a . 
+        . 4 a a a a a a a a a a a 4 . 
+        . 4 2 4 a a a a a a a 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . 4 2 . . . . . . . . 4 2 . . 
+        . 4 . . . . . . . . . 4 . . . 
+        `],
+    250,
+    false
+    )
+    pause(501)
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . 
+        . . . . a a a a a a a . . . . 
+        . . a a a a a a a a a a a . . 
+        . a a a . . . . . . . a a a . 
+        . a a . . . . . . . . . a a . 
+        a a . . . . . . . . . . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        . a a . . . . . . . . . a a a 
+        . a a a . . . . . . . a a a . 
+        . 4 a a a a a a a a a a a 4 . 
+        . 4 2 4 a a a a a a a 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . . 2 4 . . . . . . . . 2 4 . 
+        . . 2 . . . . . . . . . 2 4 . 
+        `,img`
+        . . . . . . . . . . . . . . . 
+        . . . . a a a a a a a . . . . 
+        . . a a a a a a a a a a a . . 
+        . a a a . . . . . . . a a a . 
+        . a a . . . . . . . . . a a . 
+        a a . . . . . . . . . . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . a . . . . . a . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        a a . . . . . . . . . . . a a 
+        . a a . . . . . . . . . a a a 
+        . a a a . . . . . . . a a a . 
+        . 4 a a a a a a a a a a a 4 . 
+        . 4 2 4 a a a a a a a 4 2 4 . 
+        . 4 2 4 . . . . . . . 4 2 4 . 
+        . 4 2 4 . . . . . . . . 2 4 . 
+        . 4 2 4 . . . . . . . . 2 4 . 
+        . . 2 4 . . . . . . . . 2 . . 
+        `],
+    250,
+    true
+    )
+})
 let mySprite: Sprite = null
+game.splash("Trials of Jumping Academy")
 mySprite = sprites.create(img`
     . . . . a a a a a a a . . . . 
     . . a a a a a a a a a a a . . 
@@ -24,9 +225,9 @@ mySprite = sprites.create(img`
     . 4 2 4 . . . . . . . 4 2 4 . 
     . . 2 4 . . . . . . . . 2 4 . 
     . . 2 . . . . . . . . . 2 4 . 
-    `, SpriteKind.Player)
+    `, SpriteKind.Jumper)
 tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
-mySprite.y = 219
+mySprite.y = 270
 mySprite.ay = 100
 controller.moveSprite(mySprite, 100, 0)
 scene.setBackgroundColor(1)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,21 +8,27 @@ namespace myTiles {
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`0a0011000000000000000000000000000000000000000000000303000000000000000000000000020200000000000000000000000202000000020200000000000000000000000000000000020200000000000000000000000202000000000000000000000000020200000000000202000000000000000000000000000002020000000000000000000000020200000000000000000000000002020000000000000000000001010101010101010101`, img`
+            case "level1":return tiles.createTilemap(hex`0a00130000000000000000000000000000000000000000000303000000000000000000000000020200000000000000000000000000000000020200000000000000000000000000000000040000000000000000000502000000000000000000000002020000000000000000000000000202000000000000000000000000000000000202000000000000000000000000000002020000000000000000000000020200000000000000000000000002020000000000000000000001010101010101010101`, img`
 . . . . . . . . . . 
 . . . . . . . . . . 
-. 2 2 . . . . . . . 
-. . . . . 2 2 . . . 
-. . . . . . . . 2 2 
-. . . 2 2 . . . . . 
-. . . . . . . . . . 
-. 2 2 . . . . . . . 
+2 2 . . . . . . . . 
 . . . . 2 2 . . . . 
-. . . . . . . . 2 2 
+. . . . . . . . . . 
+. . 2 2 . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 . . . . . . . . 
+. . . 2 2 . . . . . 
+. . . . . . . 2 2 . 
+. . . . . . . . . . 
 . . . . . 2 2 . . . 
 . . . . . . . . . . 
 . 2 2 . . . . . . . 
@@ -30,7 +36,7 @@ namespace myTiles {
 . . . . . . . . 2 2 
 . . . . . . . . . . 
 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
         }
         return null;
     })
@@ -44,6 +50,10 @@ namespace myTiles {
             case "tile2":return tile2;
             case "myTile3":
             case "tile5":return tile5;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile1":
+            case "tile3":return tile3;
         }
         return null;
     })
